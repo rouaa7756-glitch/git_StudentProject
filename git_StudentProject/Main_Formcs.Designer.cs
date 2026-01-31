c1a5ed7e0111ed6f21cf590187c74c63cfdde4cc
Rouaa <Rouaa@gmail.com>
2026-01-31 23:37:00 +0300

    Change Main_Form background color

--- git_StudentProject/Main_Formcs.Designer.cs
+++ git_StudentProject/Main_Formcs.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main_Formcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1019, 906);
+            this.Name = "Main_Formcs";
             this.Text = "Main_Formcs";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
