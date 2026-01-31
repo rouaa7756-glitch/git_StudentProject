34600e691f5783eebe842b17218c0a579accf89c
Rouaa <Rouaa@gmail.com>
2026-01-31 23:22:34 +0300

    Changes Login_Form change lable 2 fontcolor

--- git_StudentProject/Login_Form.Designer.cs
+++ git_StudentProject/Login_Form.Designer.cs
@@ -104,6 +104,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(330, 389);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 29);
