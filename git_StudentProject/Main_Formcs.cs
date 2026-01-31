098bb06cd2cb250636a3f2d87b25287d47d7f695
Rouaa <Rouaa@gmail.com>
2026-01-31 23:36:00 +0300

    Create Main_Form

---
+++ git_StudentProject/Main_Formcs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace git_StudentProject
+{
+    public partial class Main_Formcs : Form
+    {
+        public Main_Formcs()
+        {
+            InitializeComponent();
+        }
+    }
+}
