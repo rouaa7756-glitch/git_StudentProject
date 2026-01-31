3916cb0e2d7e80e7e4e66b2afd00b63c7b8b9803
Rouaa <Rouaa@gmail.com>
2026-02-01 01:01:24 +0300

    change main_form adding student_bt

--- git_StudentProject/Main_Formcs.Designer.cs
+++ git_StudentProject/Main_Formcs.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.Students = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.Students.SuspendLayout();
             this.SuspendLayout();
             // 
             // Students
             // 
             this.Students.BackColor = System.Drawing.Color.White;
+            this.Students.Controls.Add(this.button2);
             this.Students.Controls.Add(this.button1);
             this.Students.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Students.Location = new System.Drawing.Point(27, 37);
@@ -53,6 +55,15 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(483, 145);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Main_Formcs
             // 
@@ -73,5 +84,6 @@
 
         private System.Windows.Forms.GroupBox Students;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
